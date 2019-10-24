0f05c941f867ac15587d19cf1c398e5507db5d17
Maksym Bezrodnyi <m.bezrodnyi@visco.no>
2019-10-24 17:49:53 +0300

    parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,47 @@ import { Component, ChangeDetectorRef } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  sideBarParams: string[] = ['Møterom', 'Arbeidssplass', 'Klima', 'Lys', 'Kantine', 'Parkering', 'Mat', 'Tilbakemelding'];
+  sideBarParams: string[] = ['MГёterom', 'Arbeidssplass', 'Klima', 'Lys', 'Kantine', 'Parkering', 'Mat', 'Tilbakemelding'];
   floors: string[] = ['1', '2', '3', '4', '5', '6', '7', 'U1', 'U2', 'U3'];
   floor: string = '1';
   objectToHighlight: any;
+  position: any;
   inputId: string = "01-X09-B";
   inputColor = "#4000ff";
   inputArea = "zones";
 
   constructor(private ref: ChangeDetectorRef) {
+    let defaultFloor = '1';
+    let paramsObj = this.paramsParser(window.location.search.slice(1));
+    console.log('-paramsObj:', paramsObj);
+
+    this.position = paramsObj.position;
+   
+    if (paramsObj.floor && this.floors.indexOf(paramsObj.floor) > -1) {
+        this.floor = paramsObj.floor;
+    } else {
+      this.floor = defaultFloor;
+      console.log(`--Bad params. Default floor: ${defaultFloor} was loaded`);      
+    }
+  }
+
+  paramsParser(query) {
+    let paramPairsArr = query.split('&');
+    let paramsObj: any = {};
+
+    paramPairsArr.forEach(pair => {
+    let splitedPair = pair.split('=');
+    let value: any = {};
+    if (splitedPair[0] === 'position') {
+      let valArr = splitedPair[1].split(',');
+      value.top = valArr[0]/100;
+      value.left = valArr[1]/100;
+    } else {
+      value = splitedPair[1]
+    }
+    paramsObj[splitedPair[0]] = value;
+    })
+    return paramsObj;
   }
 
   highLight(): void {
