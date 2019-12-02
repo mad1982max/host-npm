2c0e1838b278aaa6d81202a2db7cbde4b81880d8
Maksym Bezrodnyi <m.bezrodnyi@visco.no>
2019-12-02 14:39:44 +0200

    ws blind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,14 +62,17 @@ export class AppComponent implements OnInit {
   inputCoordParam = 'CMX';
   x = 184.41725;
   y = 67.89225;
+  //any?
   hostBlinds: any;
   hostRooms: any;
   hostRoomsWithNegativeFB: any;
-  blindSuccessObj;
-  ws;
-  wsChanges;
+  blindSuccessObj: any;
+  ws: any;
+  wsChanges: any;
   mode = 'application';
   wsAttemptsCounter = 0;
+  idForBlindsGroup = 0;
+  clickedBlindsArr = [];
 
   constructor(
     private ref: ChangeDetectorRef,
@@ -78,7 +81,7 @@ export class AppComponent implements OnInit {
   // parse params from URL
   paramsParser(query: string): ParamsObj {
     const paramPairsArr = query.split('&');
-    const paramsObj: any = {};
+    const paramsObj = {};
 
     paramPairsArr.forEach(pair => {
     const splitedPair = pair.split('=');
@@ -218,79 +221,175 @@ export class AppComponent implements OnInit {
   }
 
   getBlindsObj(blindsObj: BlindsObj): void {
+  
     let action: string;
+    //open,close in key?
     if (blindsObj.hasOwnProperty('close')) { action = 'close'; }
     if (blindsObj.hasOwnProperty('open')) { action = 'open'; }
     const blindsArr = (blindsObj[action] as []);
     const answersArr = [];
-    let queryCounter = 0;
-    const qeries = blindsArr.length;
-
-    const apiAction = action === 'open' ? 'open' : 'closed';
+    
+    const apiAction = action === 'open' ? 'open' : 'closed'; //fix naming  
+    const blindObj = {blindsArr, apiAction};
+  
+    let currentBlindId = ++this.idForBlindsGroup;
+    this.clickedBlindsArr.push({id: currentBlindId, blindsArr, apiAction, answersArr});
+    console.log('**group of clicked blinds - ', this.clickedBlindsArr);
+  
+    let tick = setTimeout(() => {
+      let blindGroupInd = this.clickedBlindsArr.findIndex(item => {
+        return item.id === currentBlindId ;
+      });
+      console.log('-- if blind in clicked group', blindGroupInd);
+      if (blindGroupInd > -1) {
+        console.log('timer is finished!!! return ico and show red circle');
+        this.blindSuccessObj = Object.assign({}, blindObj, {status: false, disabled: true});
+        this.clickedBlindsArr.splice(blindGroupInd, 1);
+      }
+    }, 180000);
+  
     blindsArr.forEach(id => {
-      console.log('*** post to serverHOST, blind:', (id as any).bimBlindId, apiAction);
-      const blindObj = {blindsArr, apiAction};
-      this.dataService.blindAction((id as any).bimBlindId, apiAction)
+      let currentBlind = (id as any).bimBlindId;
+      console.log('%c ++ post to server_ HOST, blind:', 'background:#b3ffe6: color: black', (id as any).bimBlindId, apiAction);  
+  
+      this.dataService.blindAction(currentBlind, apiAction)
       .subscribe(
         data => {
-          console.log('[response postHOST]', data);
-          const correlationId = (data as any).correlationId;
-          let counter = 0;
-
-          const timer = setInterval(() => {
-
-            this.dataService.checkBlindStatus(correlationId)
-            .subscribe(
-              data => {
-
-                counter++;
-                // console.log('request counter - from 3', counter);
-                console.log('[response confirm get in HOST]', data);
-
-                if ((data as any).status === 'SUCCESS') {
-                  queryCounter++;
-                  clearInterval(timer);
-                  answersArr.push('success');
-                  console.log('%c BLIND_HOST - ACTION DONE', 'background: #0000cc; color: white');
-                } else if ((data as any).status === 'FAILED') {
-                  queryCounter++;
-                  clearInterval(timer);
-                  console.log('%c BLIND_HOST - ACTION FAILED', 'background: #0000cc; color: white');
-                  answersArr.push('failed');
-                } else {
-                  if (counter > 5) {
-                    queryCounter++;
-                    console.log('%c BLIND_HOST - ACTION ERROR: too much attempts', 'background: #0000cc; color: white');
-                    clearInterval(timer);
-                    answersArr.push('timeOut');
-                  }
-                }
-                if (queryCounter === qeries) {
-                  const statusFailed = answersArr.some(item => item === 'failed');
-                  if (statusFailed) {
-                    this.blindSuccessObj = Object.assign({}, blindObj, {status: false, disabled: true});
-                    console.log(this.blindSuccessObj);
-                  } else {
-                    const status = !answersArr.some(item => item === 'timeOut');
-                    this.blindSuccessObj = Object.assign({}, blindObj, {status});
-                    console.log(this.blindSuccessObj);
-                  }
-                }                                
-              },
-              error => {
-                console.log('--ERROR in blind action  confirm get', error.message);
-              }
-            );
-          }, 3000);
+          console.log(' ++ [response post_HOST]', data);
         },
         error => {
-          console.log('--ERROR in blind action post', error.message);
+          console.log(' ++ ERROR in blind action post', error.message);
           this.blindSuccessObj = Object.assign({}, blindObj, {status: false, disabled: true});
         });
-    });
+    });  
   }
 
-  wsFunc(floor: string) {
+  showChangesWs(data) {
+    console.log('changes: ', data);
+    if (data.logicalRoomId) {
+      this.wsChanges = data;
+
+    } else if (data.bimBlindId) {
+      console.log('changes in data.bimBlindId', this.clickedBlindsArr, data.bimBlindId);
+      
+      let blindGroupInd = this.clickedBlindsArr.findIndex(item => {
+        let arrOfbimBlindId = item.blindsArr.map(val => val.bimBlindId);
+       
+        return arrOfbimBlindId.indexOf(data.bimBlindId) > -1;
+      });
+
+      let curBlindGroup = Object.assign({}, this.clickedBlindsArr[blindGroupInd]);
+
+      if (data.changes.originReportedValue === 'inactive') {
+        //this.blindSuccessObj = Object.assign({}, curBlindGroup, {apiAction: data.changes.reportedValue, status: true, disabled: true});
+        console.log('INACTIVE: not implemented yet');
+      }
+
+      if(blindGroupInd > -1) {
+          console.log('blind is in clickedArr');
+          curBlindGroup.answersArr.push(data.changes.reportedValue);
+
+          if (curBlindGroup.blindsArr.length === curBlindGroup.answersArr.length && curBlindGroup.answersArr.every(item => item === curBlindGroup.answersArr[0])) {
+            this.blindSuccessObj = Object.assign({}, curBlindGroup, {apiAction: data.changes.reportedValue, status: true});
+            this.clickedBlindsArr.splice(blindGroupInd, 1);
+          } else if (curBlindGroup.blindsArr.length === curBlindGroup.answersArr.length && !curBlindGroup.answersArr.every(item => item === curBlindGroup.answersArr[0])){
+            this.blindSuccessObj = Object.assign({}, curBlindGroup, {status: false});
+          }
+
+      } else {
+          console.log('blind is NOT in clickedarr');         
+          let indexOF_ = data.bimBlindId.lastIndexOf('_');
+          let cuttedbimBlindId = data.bimBlindId.slice(0, indexOF_);
+          console.log('---cuttedbimBlindId', cuttedbimBlindId);
+          
+          const allBlinds = this.hostBlinds
+            .filter(item => {
+                const tag = item.bimBlindId;
+                return tag.indexOf(cuttedbimBlindId) > -1;
+            })
+            .map(item => ({bimBlindId: item.bimBlindId, objectName: item.objectName}));
+
+          let obj = {blindsArr: allBlinds, apiAction: data.changes.reportedValue}
+          this.blindSuccessObj = Object.assign({}, obj, {status: true});
+      }
+      // ++ [ws message_MODULE] : {"floor":4,"bimBlindId":"237.04-X01-A-XM601_","changes":{"originReportedValue":"inactive","reportedValueUpdatedAt":"2019-11-28T15:36:33.492Z","technicalZoneId":"04_X01_A","buildingIdFloor":"finansparken@4","reportedValue":"closed","objectName":".x237_04_X01_A_XM601_STA","bimBlindId":"237.04-X01-A-XM601_","floor":4,"buildingId":"finansparken"}}
+    }
+  }
+
+  // getBlindsObj(blindsObj: BlindsObj): void {
+  //   let action: string;
+  //   if (blindsObj.hasOwnProperty('close')) { action = 'close'; }
+  //   if (blindsObj.hasOwnProperty('open')) { action = 'open'; }
+  //   const blindsArr = (blindsObj[action] as []);
+  //   const answersArr = [];
+  //   let queryCounter = 0;
+  //   const qeries = blindsArr.length;
+
+  //   const apiAction = action === 'open' ? 'open' : 'closed';
+  //   blindsArr.forEach(id => {
+  //     console.log('*** post to serverHOST, blind:', (id as any).bimBlindId, apiAction);
+  //     const blindObj = {blindsArr, apiAction};
+  //     this.dataService.blindAction((id as any).bimBlindId, apiAction)
+  //     .subscribe(
+  //       data => {
+  //         console.log('[response postHOST]', data);
+  //         const correlationId = (data as any).correlationId;
+  //         let counter = 0;
+
+  //         const timer = setInterval(() => {
+
+  //           this.dataService.checkBlindStatus(correlationId)
+  //           .subscribe(
+  //             data => {
+
+  //               counter++;
+  //               // console.log('request counter - from 3', counter);
+  //               console.log('[response confirm get in HOST]', data);
+
+  //               if ((data as any).status === 'SUCCESS') {
+  //                 queryCounter++;
+  //                 clearInterval(timer);
+  //                 answersArr.push('success');
+  //                 console.log('%c BLIND_HOST - ACTION DONE', 'background: #0000cc; color: white');
+  //               } else if ((data as any).status === 'FAILED') {
+  //                 queryCounter++;
+  //                 clearInterval(timer);
+  //                 console.log('%c BLIND_HOST - ACTION FAILED', 'background: #0000cc; color: white');
+  //                 answersArr.push('failed');
+  //               } else {
+  //                 if (counter > 5) {
+  //                   queryCounter++;
+  //                   console.log('%c BLIND_HOST - ACTION ERROR: too much attempts', 'background: #0000cc; color: white');
+  //                   clearInterval(timer);
+  //                   answersArr.push('timeOut');
+  //                 }
+  //               }
+  //               if (queryCounter === qeries) {
+  //                 const statusFailed = answersArr.some(item => item === 'failed');
+  //                 if (statusFailed) {
+  //                   this.blindSuccessObj = Object.assign({}, blindObj, {status: false, disabled: true});
+  //                   console.log(this.blindSuccessObj);
+  //                 } else {
+  //                   const status = !answersArr.some(item => item === 'timeOut');
+  //                   this.blindSuccessObj = Object.assign({}, blindObj, {status});
+  //                   console.log(this.blindSuccessObj);
+  //                 }
+  //               }                                
+  //             },
+  //             error => {
+  //               console.log('--ERROR in blind action  confirm get', error.message);
+  //             }
+  //           );
+  //         }, 3000);
+  //       },
+  //       error => {
+  //         console.log('--ERROR in blind action post', error.message);
+  //         this.blindSuccessObj = Object.assign({}, blindObj, {status: false, disabled: true});
+  //       });
+  //   });
+  // }
+
+  wsFunc(floor: string): void {
     const that = this;
     const filter = encodeURIComponent(JSON.stringify({
       floor: +floor,
@@ -310,7 +409,8 @@ export class AppComponent implements OnInit {
         console.log(`%c [ws message_HOST] : ${event.data}`, 'background: orange; color: black');
         const WSData = JSON.parse(event.data);
         if (that.floor == WSData.floor) {
-          that.wsChanges = WSData;
+          that.showChangesWs(WSData);
+          //that.wsChanges = WSData;
         } else {
           console.log('[WS]---changes are not from this level');
         }
@@ -318,7 +418,7 @@ export class AppComponent implements OnInit {
     socket.onerror = function(event) {
       console.log('%c [ws error_HOST]', 'background: orange; color: black');
       if(that.wsAttemptsCounter <= 20) {   
-        console.log(`%c ++ [ws message_MODULE] : new attempt ${that.wsAttemptsCounter} after ERROR`, 'background: orange; color: black');
+        console.log(`%c ++ [ws message_HOST] : new attempt ${that.wsAttemptsCounter} after ERROR`, 'background: orange; color: black');
         that.wsFunc(floor);     
       } else {
         console.error('WS connection failed after 20 attempts');
